544f598c01dfd5c6bc2f8410e034cc1a0c9e3037
Sebastian Mira Pacheco <sebastianandresmira@gmail.com>
2024-07-04 01:44:37 -0400

    doc disco

--- Tareas/T3/include/Disk/disk.hpp
+++ Tareas/T3/include/Disk/disk.hpp
@@ -1,10 +1,11 @@
+// Incluye los encabezados de CGAL necesarios
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 #include <CGAL/draw_triangulation_2.h>
-
 #include <CGAL/intersections.h>
 #include <CGAL/Object.h>
 
+// Incluye otros encabezados de C++ y Boost
 #include <cassert>
 #include <cfloat>
 #include <cstdlib> 
@@ -18,12 +19,14 @@
 #include <algorithm> 
 #include <boost/property_map/property_map.hpp>
 
+// Definición de tipos utilizando el kernel de construcciones exactas e inexactas de CGAL
 typedef CGAL::Exact_predicates_inexact_constructions_kernel       K;
 typedef K::Point_2 Point_2;
 typedef K::Segment_2 Segment_2;
 typedef K::Ray_2 Ray_2;
 typedef K::Line_2 Line_2;
 
+// Definición de tipos para la triangulación de Delaunay con restricciones
 typedef CGAL::Triangulation_vertex_base_2<K>                      Vb;
 typedef CGAL::Constrained_triangulation_face_base_2<K>            Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb>               TDS;
@@ -31,7 +34,7 @@ typedef CGAL::Triangulation_data_structure_2<Vb,Fb>               TDS;
 typedef CGAL::Exact_predicates_tag Itag;
 typedef CGAL::Constrained_Delaunay_triangulation_2<K, TDS, Itag> CDT;
 
-
+// clase con la triangulación y proceso de obtencion de diagrama
 class DiskTriangulation {
     private:
         float radius, pcnt;
@@ -43,10 +46,12 @@ class DiskTriangulation {
 
 
     public:
+        // Constructor de la clase
         DiskTriangulation(float radius, int points, float pcnt);
-        void compute_voronoi();
-        void write_delaunay_off(const std::string& filename);
-        void write_voronoi_off(const std::string& filename);
-        void draw_delaunay();
-        void getCircleTriangulation(CDT& t, float r, int pointsA, float pcnt);
+        // Métodos de la clase
+        void compute_voronoi(); // Computa el diagrama de Voronoi
+        void write_delaunay_off(const std::string& filename); // Escribe la triangulación de Delaunay en un archivo OFF
+        void write_voronoi_off(const std::string& filename); // Escribe el diagrama de Voronoi en un archivo OFF
+        void draw_delaunay(); // Dibuja la triangulación de Delaunay
+        void getCircleTriangulation(CDT& t, float r, int pointsA, float pcnt); // Prototipo de función para obtener la triangulación de un disco
 };
--- Tareas/T3/src/disk.cpp
+++ Tareas/T3/src/disk.cpp
@@ -3,12 +3,20 @@
 void removeDuplicates(std::vector<Point_2>& myVector);
 
 
-// Creates a disk of radius r with a pointsA amount of points. A percentage (pcnt) of them are on the border and the rest are randomly inside.
+// getCircleTriangulation: Crea una triangulación de Delaunay para un disco de radio r
+// con pointsA puntos, de los cuales un porcentaje pcnt se encuentra en el borde y 
+// el resto dentro del disco.
+// t: La triangulación de Delaunay.
+// r: Radio del disco.
+// pointsA: Número total de puntos a insertar.
+// pcnt: Porcentaje de puntos que están en el borde.
 void DiskTriangulation::getCircleTriangulation(CDT& t, float r, int pointsA, float pcnt) {
+    // puntos en borde
     int amountBorder = pcnt*(float)pointsA;
+    // puntos en interior
     int amountInnerPoints = pointsA-amountBorder;
 
-
+    // angulo de incrementos para ingresar borde
     float angleIncrement = 2 * M_PI / (float)amountBorder;
     for (int i = 0; i < amountBorder-1; i++) {
         float angle = (float)i * angleIncrement;
@@ -18,17 +26,21 @@ void DiskTriangulation::getCircleTriangulation(CDT& t, float r, int pointsA, flo
 
         float x2 = r * cos(angleInc);
         float y2 = r * sin(angleInc);
+        // se guarda además los puntos del borde en sentido ccw
         bd_points.push_back(Point_2(x1,y1));
         if (i == amountBorder-2) bd_points.push_back(Point_2(x2,y2));
-
+        // se colocan como restricción
         t.insert_constraint(Point_2(x1,y1), Point_2(x2,y2));
     }
-
+    // verifica validez de borde
     assert(t.is_valid());
 
+    // se colocan puntos aleatorios dentro del disco 
     std::default_random_engine gen;
     gen.seed(std::time(0));
-    std::uniform_real_distribution<float> rad(0.0, r*cos(angleIncrement/2.0f)*0.95f);
+    // se hace en un rango menor al radio para asegurar triángulos interiores con circuncentro
+    // dentro del dominio
+    std::uniform_real_distribution<float> rad(0.0, r*cos(angleIncrement/2.0f)*0.9f);
     std::uniform_real_distribution<float> theta(0.0, 2 * M_PI);
     for (int i = 0; i < amountInnerPoints; i++) {
         
@@ -41,6 +53,7 @@ void DiskTriangulation::getCircleTriangulation(CDT& t, float r, int pointsA, flo
 
 }
 
+// funcion para encontrar el índice de un punto en un vector de puntos. Si no está, retorna -1
 int findInd(Point_2 const & p, std::vector<Point_2>& v) {
     for (int i = 0; i < v.size(); i++) {
         if (v[i] == p) return i;
@@ -49,20 +62,23 @@ int findInd(Point_2 const & p, std::vector<Point_2>& v) {
 }
 
 
-
+// Computa el diagrama de Voronoi para la triangulación del rectángulo guardando 
+// vectores con puntos que representan una celda de voronoi. Se guardan en ccw.
 void DiskTriangulation::compute_voronoi() {
+    // se itera sobre todos los vérices de la triangulación
+    // pues son centros de sectores de voronoi
     for (auto vit = disk.finite_vertices_begin(); vit != disk.finite_vertices_end(); ++vit) {
         std::vector<Point_2> circ_centers;
 
-        
+        // Se circula a través de las caras adyacentes al vértice
         CDT::Face_circulator fcirc = disk.incident_faces(vit), done(fcirc);
         if (fcirc != 0) {
             do {
+                // Si la cara actual no es infinita se procede agregando el circuncentro simplemente
                 if (!disk.is_infinite(fcirc)) {
                     Point_2 circ_center = CGAL::circumcenter(disk.triangle(fcirc));
-                    
-                    
                     circ_centers.push_back(circ_center);
+                // si es infinita se agregan los puntos medios de los bordes adyacentes al vertice revisado además del mismo vértice
                 } else {
                     int thisPind = findInd(Point_2(vit->point()), bd_points);
                     int prevPind = thisPind == 0? bd_points.size()-1 :thisPind-1;
@@ -78,8 +94,10 @@ void DiskTriangulation::compute_voronoi() {
             } while (++fcirc != done);
         }
         
+        // por seguridad se remueven duplicados
         removeDuplicates(circ_centers);
 
+        // se agrega la celda de voronoi
         voronoi_segments.push_back(circ_centers);
 
         
@@ -87,12 +105,16 @@ void DiskTriangulation::compute_voronoi() {
 }
 
 
-
+// DiskTriangulation: Constructor que inicializa la triangulación del disco y computa su diagrama de Voronoi.
+// radius: Radio del disco.
+// points: Número de puntos a insertar.
+// pcnt: Porcentaje de puntos que están en el borde.
 DiskTriangulation::DiskTriangulation(float radius, int points, float pcnt) : radius(radius), points(points), pcnt(pcnt) {
     getCircleTriangulation(disk, radius, points, pcnt);
     compute_voronoi();
 }
 
+// dibuja la triangulación con cgal, para debug solamente
 void DiskTriangulation::draw_delaunay() {
     if (disk.is_valid()) {
         std::unordered_map<CDT::Face_handle, bool> in_domain_map;
@@ -101,15 +123,21 @@ void DiskTriangulation::draw_delaunay() {
     }
 }
 
-
+//  Dibuja la triangulación de Delaunay en un archivo OFF
+// filename: Nombre del archivo donde se guardará la triangulación.
 void DiskTriangulation::write_delaunay_off(const std::string& filename) {
+    // abre archivo
     std::ofstream out(filename);
+    // primera linea
     out << "OFF\n";
 
-    // Escribir los vértices
+    // Escribir los vértices una única vez
     std::map<Point_2, int> vertex_map;
     int index = 0;
+    
+    // se escribe la 2da linea de off: vertices, caras y segmentos
     out << disk.number_of_vertices() << " " << disk.number_of_faces() << " 0\n";
+    // a cada vertice le asigna un índice en el mapa
     for (auto vit = disk.finite_vertices_begin(); vit != disk.finite_vertices_end(); ++vit) {
         Point_2 p = vit->point();
         out << p.x() << " " << p.y() << " 0\n";
@@ -125,33 +153,39 @@ void DiskTriangulation::write_delaunay_off(const std::string& filename) {
         }
         out << "\n";
     }
-
+    // cierra archivo
     out.close();
 }
 
+// Escribe el diagrama de Voronoi en un archivo OFF
+// Parámetros:
+// - filename: Nombre del archivo donde se guardará el diagrama de Voronoi
 void DiskTriangulation::write_voronoi_off(const std::string& filename) {
+    // abre archivo
     std::ofstream out(filename);
+    // primera linea
     out << "OFF\n";
-
     
-
+    // en este caso se decidió escribir vértices repetidos en el off pues se tiene un
+    // vector de vectores de puntos representando las celdas, con los puntos ya en orden
     int index = 0;
     int vertex_amount = 0;
+    // se cuentan vertices
     for (const auto& sector : voronoi_segments) {
         for (const auto& vert : sector) {
             vertex_amount++;
         }
     }
-
+    // se escriben cantidad de vertices y caras
     out << vertex_amount << " " << voronoi_segments.size() << " 0\n";
 
-
+    // se escriben los vertices
     for (const auto& sector : voronoi_segments) {
         for (const auto& vert : sector) {
             out << vert.x() << " " << vert.y() << " 0\n";
         }
     }
-
+    // se escriben las caras
     for (const auto& sector : voronoi_segments) {
         out << sector.size();
         for (const auto& vert : sector) {
@@ -164,25 +198,25 @@ void DiskTriangulation::write_voronoi_off(const std::string& filename) {
     out.close();
 }
 
-
+// Elimina duplicados de un vector de puntos
+// Parámetros:
+// - myVector: Vector de puntos del cual se eliminarán duplicados
 void removeDuplicates(std::vector<Point_2>& myVector) { 
     std::unordered_set<Point_2> seen; 
   
-    // Using remove_if to eliminate duplicates and get the 
-    // new end iterator 
+    // Se remueve si el punto revisado ya está en seen
     auto newEnd = remove_if( 
         myVector.begin(), myVector.end(), 
         [&seen](Point_2& value) { 
-            // Checking if value has been seen; if not, add 
-            // to seen and keep in vector 
+            // Checkea si está en seen o no 
             if (seen.find(value) == seen.end()) { 
                 seen.insert(value); 
-                return false; // Don't remove the item 
+                return false; // No se remueve si se llega al final de seen sin verlo
             } 
-            return true; // Remove the item 
+            return true; // Se remueve en caso contrario
         }); 
   
-    // Erase the non-unique elements 
+    // Se aplica la función definida para la condición de eliminación en newEnd 
     myVector.erase(newEnd, myVector.end()); 
 } 
 
